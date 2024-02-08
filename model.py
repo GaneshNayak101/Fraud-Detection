9aa0ca43bf368305f44cee3b6fdcb0da453912ab
GaneshNayak101 <ganesh535nayak@gmail.com>
2024-02-08 18:17:06 +0100

    3 new ML model code

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
 print("Adding all ML realted code here")
+
+
+def model1():
+    print("addinng code for model 1")
+
+def model2():
+    print("addinng code for model 2")
+
+def model2():
+    print("addinng code for model 3")
